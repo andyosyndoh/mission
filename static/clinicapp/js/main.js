990addd5562995325de542dcf8813ff5bdc12e3a
andyosyndoh <andyovvo8@gmail.com>
2025-04-18 13:18:51 +0300

    chore: added new images

--- static/clinicapp/js/main.js
+++ static/clinicapp/js/main.js
@@ -167,7 +167,6 @@
 				$('.number').each(function(){
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
 					$this.animateNumber(
 					  {
 					    number: num,
