dab2a605412c1378191dfab42b19fa76e2b5c52e
adaken4 <adakennedy@outlook.com>
2025-04-23 08:22:33 +0300

    [update]: update hospital email for forms submissions

--- clinic/settings.py
+++ clinic/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "adakennedy6@gmail.com"
-EMAIL_HOST_PASSWORD = "zgtn lboq nxst srvv"
+EMAIL_HOST_USER = "hmissionhospital@gmail.com"
+EMAIL_HOST_PASSWORD = "ytqf zrkm vqbb tnaf"
