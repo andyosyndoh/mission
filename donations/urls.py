774dab388ecf07ef6f710b562ca9a4ad1dcdef72
masman <asmanodeny@gmail.com>
2025-04-29 14:32:36 +0300

    added urls

--- donations/urls.py
+++ donations/urls.py
@@ -1,13 +1,12 @@
-# donations/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'donations'  # Namespace declaration
+app_name = 'donations'
 
 urlpatterns = [
     path('initiate/', views.initiate_donation, name='initiate_donation'),
-    path('callback/', views.payment_callback, name='payment_callback'),
     path('success/', views.payment_success, name='payment_success'),
     path('failed/', views.payment_failed, name='payment_failed'),
+    path('callback/', views.payment_callback, name='payment_callback'),
     path('ipn/', views.ipn_handler, name='ipn_handler'),
 ]
