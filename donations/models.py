d94cb0a1fce7ac3211ee8469952c7fe457a03e85
Malika7188 <asmanodeny@gmail.com>
2025-05-24 22:37:39 +0300

    updated migration and models fields

--- donations/models.py
+++ donations/models.py
@@ -17,6 +17,7 @@ class Donation(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -26,6 +27,7 @@ class Donation(models.Model):
     pesapal_transaction_id = models.CharField(max_length=255, blank=True)
     status = models.CharField(max_length=20, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return f"{self.full_name} - {self.amount} {self.currency}"
